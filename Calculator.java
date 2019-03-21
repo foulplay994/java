bcf1d4bbfd9cbff06553d527d538b383581c059e
foulplay994 <foulplay994@gmail.com>
2019-03-21 17:55:38 +0200

    last of

--- Calculator.java
+++ Calculator.java
@@ -1,44 +1,44 @@
 public class Calculator {
 	
-	private double result; 
+	private double result;  // переменная результата
 	
-	public void add(double ... params) {		//Сложение
+	public void add(double ... params) {		// сложение
 		for (Double param : params) {
 			this.result += param;
 		}
 	}
 	
-	public void dec(double ... params) {		//Вычитание    
+	public void dec(double ... params) {		// вычитание    
 		for (Double param : params) {
 			this.result=params[0]-params[1];
 		}
 	}
 	
-	public void x(double ... params) {			//Умножение    
+	public void x(double ... params) {			// умножение    
 		for (Double param : params) {
 			this.result=params[0]*params[1];
 		}
 	}
 	
-	public void part(double ... params) {		//Деление    
+	public void part(double ... params) {		// деление    
 		for (Double param : params) {
 			this.result=params[0]/params[1];
 		}
 	}	
 	
-	public void sqt(double ... sparams) {		//Степень    
+	public void sqt(double ... sparams) {		// степень    
 		for (Double sparam : sparams) {
 			this.result=Math.pow(sparams[0],sparams[1]);
 		
 		}
 	}	
 	
-    public double getResult() {
+    public double getResult() {					// вывод результата
 		return this.result;
 	}
 	
 	
-	public void cleanResult() {
+	public void cleanResult() {					// очистка результата
 		this.result = 0;
 	}
 }
--- Runner.java
+++ Runner.java
@@ -1,23 +1,24 @@
 import java.util.Scanner;
 
-public class InteractRunner {
+public class Runner {
+
+public static String first, second, lastCheck="no", lastResult="0";
+	// первый и второй аргумент, проверка последнего результата, последний результат
 	
 	public static void main(String[] arg) {
 		Scanner reader = new Scanner(System.in);
 		try {
 			Calculator calc = new Calculator();
-			String lastResult=null;						// последний результат
-			String lastCheck="0";					 	// проверка последнего результата
-			String exit = "no";							// переменная для выхода
+			String exit = "no";		
 			while (!exit.equals("yes")) {
-				String first, second = "";
+				
 				
 				System.out.println("Use last result? Type Y to accept : ");   // первый аргумент
 				lastCheck = reader.next();
 				if(lastCheck.equals("y")) {
 					System.out.println("Last result was taken like first arg ");    
 					 System.out.println("It was " + lastResult);	
-						first=lastResult;											// первый аргумент по последнему результату
+					  first=lastResult;									// первый аргумент по последнему результату
 					System.out.println("Enter second arg : ");	// второй аргумент
 				second = reader.next(); }
 				
@@ -26,23 +27,26 @@ public class InteractRunner {
 				first = reader.next();						 
 				System.out.println("Enter second arg : ");	// второй аргумент
 				second = reader.next(); }
-				System.out.println("Enter type : add dec x part sqt. Default is add");  // список действий
-				String ctype = reader.next(); 
+				System.out.println("Enter type : 1)add  2)dec  3)x  4)part  5)sqt ");  // список действий
+				String runType = reader.next(); 
 				
-				if(ctype.equals("1")) {												// условия выбора действия
+				if(runType.equals("1")) {												// условия выбора действия
 				calc.add(Double.valueOf(first), Double.valueOf(second)); }
-				else if(ctype.equals("2")) {	
+				else if(runType.equals("2")) {	
 				calc.dec(Double.valueOf(first), Double.valueOf(second)); }
-				else if(ctype.equals("3")) {
+				else if(runType.equals("3")) {
 				calc.x(Double.valueOf(first), Double.valueOf(second)); }
-				else if(ctype.equals("4")) {	
+				else if(runType.equals("4")) {	
 				calc.part(Double.valueOf(first), Double.valueOf(second)); }
-				else if(ctype.equals("5")) {	
+				else if(runType.equals("5")) {	
 				calc.sqt(Double.valueOf(first), Double.valueOf(second)); }
 						
-				System.out.println("Result : " + calc.getResult());
+				PrintNClean();
+						
+				System.out.println("Result : " + calc.getResult());					
 				lastResult = String.valueOf(calc.getResult());						// запись последнего результата
-				calc.cleanResult(); ctype = "";
+				calc.cleanResult(); runType = "";
+				
 				System.out.println("Exit : yes / no ");
 				exit = reader.next();
 			}
@@ -50,4 +54,11 @@ public class InteractRunner {
 			reader.close();
 		}
 	}
-}
+
+	
+	
+	public static void PrintNClean() {
+	System.out.println("Test Print ");
+	}
+	
+	}
